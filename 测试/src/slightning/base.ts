55084b3d210fda351e202ab389d7c553b4ab2b30
SLIGHTNING <3157420882@qq.com>
2025-05-10 17:53:55 +0800

    调整测试用例

--- 测试/src/slightning/base.ts
+++ 测试/src/slightning/base.ts
@@ -1,4 +1,5 @@
-import { addCheck, addThisForMethods, AnyType, ArrayType, BooleanType, Color, emit, exportWidget, flattenEventSubTypes, FunctionType, generateBlockForProperties, generateMethodForFunctions, getSuperWidget, InstanceType, IntegerType, Logger, MethodBlockParam, ObjectType, StringType, transformIcons, transformIconsExceptWidgetIcon, transformMethodsCallbackFunctionsToCodeBlocks, transformMethodsCallbackFunctionsToEvents, transformMethodsThrows, Types, VoidType } from "slightning-coco-widget"
+import { addCheck, addThisForMethods, ArrayType, CoCo, Color, CreationProject, emit, exportWidget, flattenEventSubTypes, FunctionType, generateBlockForProperties, generateMethodForFunctions, getSuperWidget, IntegerType, Logger, MethodBlockParam, ObjectType, StringType, transformIcons, transformIconsExceptWidgetIcon, transformMethodsCallbackFunctionsToCodeBlocks, transformMethodsCallbackFunctionsToEvents, transformMethodsThrows, Types } from "slightning-coco-widget"
+import _ from "lodash"
 
 const types: Types = {
     type: "SLIGHTNING_TEST_BASE_WIDGET",
@@ -13,170 +14,121 @@ const types: Types = {
     },
     properties: [
         {
-            key: "width",
-            label: "宽度",
-            type: new IntegerType({
-                defaultValue: 360
-            })
-        }, {
-            key: "height",
-            label: "高度",
-            type: new IntegerType({
-                defaultValue: 360
-            })
-        }, {
-            key: "testProperty",
-            label: "测试属性",
+            key: "testPropertyDefaultKeyDefaultCalculateMethod",
+            label: "测试属性默认键默认计算函数",
             type: new StringType({
-                defaultValue: "默认值"
+                defaultValue: "测试属性默认键默认计算函数默认值"
             })
+        }, {
+            key: "testPropertyCustomKeyDefaultCalculateMethod",
+            label: "测试属性自定义键默认计算函数",
+            type: new StringType({
+                defaultValue: "测试属性自定义键默认计算函数默认值"
+            }),
+            blockOptions: {
+                get: {
+                    key: "customKeyDefaultGet"
+                },
+                set: {
+                    key: "customKeyDefaultSet"
+                }
+            }
+        }, {
+            key: "testPropertyCustomKeyCustomCalculateMethod",
+            label: "测试属性自定义键自定义计算函数",
+            type: new StringType({
+                defaultValue: "测试属性自定义键自定义计算函数默认值"
+            }),
+            blockOptions: {
+                get: {
+                    key: "customKeyCustomGet"
+                },
+                set: {
+                    key: "customKeyCustomSet"
+                }
+            }
         }
     ],
-    methods: [
-        {
-            blockOptions: {
-                icon: "icon-widget-radio",
-                color: Color.RED
-            },
-            contents: [
-                {
-                    label: "测试组",
-                    contents: [
-                        {
-                            label: "测试子组",
-                            contents: [
-                                {
-                                    key: "testGroupMethod",
-                                    label: "测试组方法",
-                                    block: [
-                                        MethodBlockParam.THIS, MethodBlockParam.METHOD
-                                    ]
-                                }
-                            ]
-                        }
-                    ]
-                }, {
-                    key: "testTypes",
-                    label: "测试方法",
-                    block: [
-                        MethodBlockParam.METHOD, "列表", {
-                            key: "array",
-                            label: "列表",
-                            type: new ArrayType({
-                                itemType: new StringType()
-                            })
-                        }, "字典", {
-                            key: "object",
-                            label: "字典",
-                            type: new ObjectType({
-                                propertiesType: {
-                                    "整数": new IntegerType(),
-                                    "字符串": new StringType()
-                                }
-                            })
-                        }
-                    ]
-                }, {
-                    key: "testMethod",
-                    label: "测试方法",
-                    block: [
-                        "使", MethodBlockParam.THIS, MethodBlockParam.METHOD, "抛出异常"
-                    ],
-                    returns: new BooleanType(),
-                    throws: new StringType()
-                }, {
-                    contents: [
-                        {
-                            key: "testEvent",
-                            label: "测试事件",
-                            block: [
-                                MethodBlockParam.METHOD
-                            ]
-                        }
-                    ]
-                }
-            ]
-        }, {
-            label: "承诺",
-            blockOptions: {
-                color: Color.CYAN
-            },
-            contents: [
-                {
-                    key: "newPromise",
-                    label: "创建承诺",
-                    block: [
-                        MethodBlockParam.METHOD, {
-                            key: "callback",
-                            label: "回调",
-                            type: new FunctionType({
+    methods: [{
+        blockOptions: {
+            icon: "icon-widget-radio",
+            color: Color.RED
+        },
+        contents: [
+            {
+                label: "测试组",
+                contents: [
+                    {
+                        label: "测试子组",
+                        contents: [
+                            {
+                                key: "testGroupMethod",
+                                label: "测试组方法",
                                 block: [
+                                    MethodBlockParam.THIS, MethodBlockParam.METHOD
+                                ]
+                            }
+                        ]
+                    }, {
+                        label: "测试无标签组",
+                        contents: [
+                            {
+                                contents: [
                                     {
-                                        key: "resolve",
-                                        label: "解决",
-                                        type: new FunctionType({
-                                            block: [
-                                                "结果", {
-                                                    key: "value",
-                                                    label: "结果",
-                                                    type: new AnyType({
-                                                        defaultValue: "结果"
-                                                    })
-                                                }
-                                            ]
-                                        })
-                                    }, {
-                                        key: "reject",
-                                        label: "拒绝",
-                                        type: new FunctionType({
-                                            block: [
-                                                "原因", {
-                                                    key: "reason",
-                                                    label: "原因",
-                                                    type: new AnyType({
-                                                        defaultValue: "原因"
-                                                    })
-                                                }
-                                            ]
-                                        })
+                                        key: "testGroupWithoutLabelMethod1",
+                                        label: "测试无标签组方法1",
+                                        block: [
+                                            MethodBlockParam.THIS, MethodBlockParam.METHOD
+                                        ]
                                     }
                                 ]
-                            })
-                        }
-                    ],
-                    returns: new InstanceType({
-                        theClass: Promise
-                    })
-                }
-            ]
-        }, {
-            label: "循环",
-            blockOptions: {
-                color: Color.CYAN
-            },
-            contents: [
-                {
-                    key: "loop",
-                    label: "循环",
-                    block: [
-                        MethodBlockParam.METHOD, {
-                            key: "body",
-                            label: "循环体",
-                            type: new FunctionType({
-                                block: [],
-                                returns: new VoidType(),
-                                throws: new InstanceType({
-                                    theClass: Error
-                                })
-                            })
-                        }
-                    ]
-                }
-            ]
-        }
-    ],
+                            }, {
+                                contents: [
+                                     {
+                                        key: "testGroupWithoutLabelMethod2",
+                                        label: "测试无标签组方法2",
+                                        block: [
+                                            MethodBlockParam.THIS, MethodBlockParam.METHOD
+                                        ]
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            }, {
+                contents: [
+                    {
+                        key: "emitTestEvent",
+                        label: "触发测试事件",
+                        block: [
+                            MethodBlockParam.METHOD
+                        ]
+                    }
+                ]
+            }, {
+                label: "内部",
+                contents: [
+                    {
+                        key: "exportedWidgetTypes",
+                        label: "导出的控件类型定义",
+                        block: [MethodBlockParam.METHOD],
+                        returns: new ObjectType()
+                    }
+                ]
+            }
+        ]
+    }],
     events: [
         {
+            key: "onCustomKeyCustomGet",
+            label: "自定义键自定义获取",
+            params: []
+        }, {
+            key: "onCustomKeyCustomSet",
+            label: "自定义键自定义设置",
+            params: []
+        }, {
             key: "onTestEvent",
             label: "测试事件",
             params: [
@@ -184,9 +136,7 @@ const types: Types = {
                     key: "function",
                     label: "函数",
                     type: new FunctionType({
-                        block: [
-                            "测试函数"
-                        ]
+                        block: []
                     })
                 }
             ]
@@ -213,16 +163,31 @@ const types: Types = {
     ]
 }
 
+let exportedWidgetTypes: CoCo.Types | CreationProject.Types
+
 class TestBaseWidget extends getSuperWidget(types) {
 
     private logger: Logger
 
-    public testProperty!: string
+    public testPropertyCustomKeyCustomCalculateMethod!: string
 
     public constructor(props: unknown) {
         super(props)
         this.logger = new Logger(types, this)
-        this.logger.log("构造函数被调用")
+    }
+
+    public customKeyCustomGet(this: this): string {
+        emit.call(this, "onCustomKeyCustomGet")
+        return this.testPropertyCustomKeyCustomCalculateMethod
+    }
+
+    public customKeyCustomSet(this: this, value: string): void {
+        emit.call(this, "onCustomKeyCustomSet")
+        this.testPropertyCustomKeyCustomCalculateMethod = value
+    }
+
+    public emitTestEvent(this: this): void {
+        emit.call(this, "onTestEvent")
     }
 
     public testEvent(this: this): void {
@@ -237,23 +202,8 @@ class TestBaseWidget extends getSuperWidget(types) {
         throw new Error("异常，这是一个测试异常")
     }
 
-    public newPromise(
-        callback: (
-            resolve: (value: unknown) => void,
-            reject: (reason: unknown) => void
-        ) => void
-    ): Promise<unknown> {
-        return new Promise(callback)
-    }
-
-    public getTestProperty(this: this): string {
-        this.logger.log(`获取 测试属性 为 ${JSON.stringify(this.testProperty)}`)
-        return this.testProperty
-    }
-
-    public setTestProperty(this: this, value: string): void {
-        this.logger.log(`设置 测试属性 为 ${JSON.stringify(value)}`)
-        this.testProperty = value
+    public exportedWidgetTypes(): CoCo.Types | CreationProject.Types {
+        return exportedWidgetTypes
     }
 }
 
@@ -281,3 +231,11 @@ exportWidget(types, TestBaseWidget, {
         ]
     }
 })
+
+exportedWidgetTypes = _.cloneDeep(
+    CoCo.widgetExports.types ??
+    CreationProject.widgetExports.type ??
+    ((): never => {
+        throw new Error("找不到导出的控件类型定义")
+    })()
+)
